58fb0ab62a95e2a1634cd2c5153bca926bf8328e
Aikhjarto <wagner-thomas@gmx.at>
2024-04-04 14:08:23 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
-from setuptools import setup
+from distutils.core import setup
+from setuptools import find_packages
 
-setup()
+setup(name="check_mqtt_topic_age",
+      version='0.1',
+      description='Nagios/Ncinga plugin to check when an MQTT topic was published',
+      author='Thomas Wagner',
+      author_email='wagner-thomas@gmx.at',
+      url='https://github.com/Aikhjarto/check_mqtt_topic_age',
+      packages=find_packages(where='src'),
+      package_dir={"": "src"},
+      install_requires=["paho-mqtt"],
+      )
